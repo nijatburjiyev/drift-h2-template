35b633ae5b0f73ba8c1c220efa1732ce7d6f6def
nijatburjiyev <nijatburjiyev@gmail.com>
2025-07-14 23:12:03 -0700

    teest

---
+++ src/main/java/com/edwardjones/drift/DriftApplication.java
@@ -0,0 +1,13 @@
+package com.edwardjones.drift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriftApplication.class, args);
+	}
+
+}
